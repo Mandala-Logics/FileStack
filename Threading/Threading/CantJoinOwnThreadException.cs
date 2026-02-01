f5642e69277c17860eca94dba148dad8ae50537e
system_boi <system_boi@operalimina.co.uk>
2026-02-01 23:42:28 +0000

    Removed threading module/project; it was included by accident.

--- Threading/Threading/CantJoinOwnThreadException.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace MandalaLogics.Threading
-{
-    public sealed class CantJoinOwnThreadException : Exception
-    {
-        public CantJoinOwnThreadException() : base() { }
-    }
-}
